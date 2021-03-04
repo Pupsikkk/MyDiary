c0107da50c7cd08501c1960636d61f1f679e3c9b
Pupsikk <orativtech7@gmail.com>
2021-03-05 00:23:40 +0200

    start

--- scripts/main.js
+++ scripts/main.js
@@ -1,5 +1,5 @@
 let field = document.querySelector(".main")
 
-for(i = 0; i < 3780; i++){
+for(i = 0; i < 300; i++){
     field.innerHTML += '<div id="main' + (i+1) + '" class="cell">' + (i+1) +'</div>' 
 }
